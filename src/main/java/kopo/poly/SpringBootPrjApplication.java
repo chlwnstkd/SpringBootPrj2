17e72e3dea795b3a172935b3b4814bff6ae7e3dc
chlwnstkd <jun1234755@naver.com>
2023-07-10 14:52:13 +0900

    2023-07-10

--- src/main/java/kopo/poly/SpringBootPrjApplication.java
+++ src/main/java/kopo/poly/SpringBootPrjApplication.java
@@ -2,6 +2,8 @@ package kopo.poly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootPrjApplication {
@@ -10,4 +12,6 @@ public class SpringBootPrjApplication {
         SpringApplication.run(SpringBootPrjApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(){return new RestTemplate();}
 }
